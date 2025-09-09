4c0f1885e256bf2e30b5a59ab075699fc322bf83
陈宇 <daboluo0524@gmail.com>
2025-09-09 15:25:35 +0800

    feat:修改方案

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 
-
-import './styles/element.scss'
+// 导入 Element Plus 样式
+import '../packages/styles/index.scss'
 
 // 导入自定义样式
 import './styles/demo.css'
