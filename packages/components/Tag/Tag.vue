fbd1e82b08ff9bcd97e926a459b05785ba088823
陈宇 <daboluo0524@gmail.com>
2025-09-18 17:39:01 +0800

    feat

--- packages/components/Tag/Tag.vue
+++ packages/components/Tag/Tag.vue
@@ -13,49 +13,5 @@ defineOptions({
 </script>
 
 <style lang="scss">
-// Tag 组件样式 - 使用 m-tag 类名隔离样式
-.m-tag.el-tag {
-  // 自定义样式可以在这里添加
-  font-size: var(--font-size-text-sm);
-  --el-tag-border-radius: 8px;
 
-  &.el-tag--primary {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-warning-primary) 10%, transparent);
-        --el-tag-border-color: color-mix(in srgb, var(--text-warning-primary) 20%, transparent);
-        --el-tag-text-color: var(--text-warning-primary);
-    }
-    &.el-tag--success {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-success-primary) 10%, transparent);
-        --el-tag-border-color: color-mix(in srgb, var(--text-success-primary) 20%, transparent);
-        --el-tag-text-color: var(--text-success-primary);
-    }
-    &.el-tag--danger {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-error-primary) 10%, transparent);
-        --el-tag-border-color: color-mix(in srgb, var(--text-error-primary) 20%, transparent);
-        --el-tag-text-color: var(--text-error-primary);
-    }
-    &.el-tag--info {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-primary) 10%, transparent);
-        --el-tag-border-color: color-mix(in srgb, var(--text-primary) 20%, transparent);
-        --el-tag-text-color: var(--text-primary);
-    }
-  &.el-tag--dark{
-    &.el-tag--warning {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-warning-primary) 10%, transparent);
-        --el-tag-border-color: transparent;
-    }
-    &.el-tag--success {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-success-primary) 10%, transparent);
-        --el-tag-border-color: transparent;
-    }
-    &.el-tag--danger {
-        --el-tag-bg-color: color-mix(in srgb, var(--text-error-primary) 10%, transparent);
-        --el-tag-border-color: transparent;
-    }
-    &.el-tag--info {
-        --el-tag-bg-color: var(--bg-tertiary-hover);
-        --el-tag-border-color: transparent;
-    }
-  }
-}
 </style>
